f6a4f69694f4edcc7531d87f5b4a260d0284306d
E. Seton Raynor <esetonraynor@gmail.com>
2017-08-28 09:06:26 -0400

    footer added, icons changed

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ function openTab(event, tabName) {
     var i, tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
+    var tabcontent= $(".tabcontent").val();
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -20,4 +21,25 @@ function openTab(event, tabName) {
 }
 
 document.getElementById("defaultOpen").click();
+
+var modal = document.getElementById('myModal');
+
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+$(document).ready(function() {
+    modal.style.display = "block";
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
